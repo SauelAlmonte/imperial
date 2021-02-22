e4225de2e4eeff17c3d08f514ab703ecb9a84610
msimbo-coding <SAlmonte@MSIMBO.org>
2021-02-22 14:12:53 -0500

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+export const Footer =() => (
+    <div className="p-16 bg-red-300">
+        Copyright 2021 Imperial App
+    </div>
+)
---
+++ src/components/MainNav.js
@@ -0,0 +1,9 @@
+export const MainNav =() => (
+    <div className="mb-5 bg-red-300">
+        <ul>
+            <li>Home</li>
+            <li>Login</li>
+            <li>Sign Up</li>
+        </ul>
+    </div>
+)
