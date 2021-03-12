d23b0b0ce4be25eb203e1893de2da2a96a804a4d
msimbo-coding <SAlmonte@MSIMBO.org>
2021-03-12 03:00:49 -0500

    update

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,12 +3,12 @@ import {Footer, MainNav} from "../components/ui";
 export const Search = () => (
     <>
         <MainNav/>
-            <div className="p-24 h-auto md:h-screen">
+            <div className="p-20 h-auto md:h-screen">
                 <div className="bg-white flex items-center rounded-full shadow-xl border-2 border-black md:max-w-screen mx-auto">
-                    <input className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" />
+                    <input className="rounded-full w-auto md:w-full py-5 px-8 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" />
 
-                        <div className="p-4">
-                            <button className="bg-gray-300 text-black rounded-full p-2 focus:outline-none w-16 h-16 flex items-center justify-center border-2 border-black hover:bg-gray-400">
+                        <div className="pl-8 p-1 md:p-1">
+                            <button className="bg-gray-300 text-black rounded-full focus:outline-none px-2 py-2 md:w-16 md:h-16 flex items-center justify-center border-2 border-black hover:bg-gray-400">
                                 Search
                             </button>
                         </div>
--- src/pages/SocialFollow.js
+++ src/pages/SocialFollow.js
@@ -15,22 +15,22 @@ export const SocialFollow = () => {
             <div className="flex flex-grow min-h-screen items-center justify-center mt-10">
                 <div className="bg-white p-10 rounded-lg shadow-2xl w-2/3 border-2 border-black">
                     <div className="social-container">
-                        <h3 className="text-black text-6xl mb-3 text-center">Social Sites</h3>
+                        <h3 className="text-black  text-base md:text-6xl mb-3 text-center">Social Sites</h3>
                         <div className="text-center my-6">
 
                             <a href="https://www.youtube.com/"
-                               className="youtube social text-gray-800 mx-6">
+                               className="text-xs md:text-xl youtube social text-gray-800 mx-6">
                                 <FontAwesomeIcon icon={faYoutube} size="6x" />
                             </a>
                             <a href="https://www.facebook.com/allmypeopleloco/"
-                               className="facebook social text-gray-800 mx-6">
+                               className="text-xs md:text-xl facebook social text-gray-800 mx-6">
                                 <FontAwesomeIcon icon={faFacebook} size="6x" />
                             </a>
-                            <a href="https://twitter.com/TheCodinCoder" className="twitter social text-gray-800 mx-6">
+                            <a href="https://twitter.com/TheCodinCoder" className="text-xs md:text-xl twitter social text-gray-800 mx-6">
                                 <FontAwesomeIcon icon={faTwitter} size="6x" />
                             </a>
                             <a href="https://www.instagram.com/thecodincoder/"
-                               className="instagram social text-gray-800 mx-6">
+                               className="text-xs md:text-xl instagram social text-gray-800 mx-6">
                                 <FontAwesomeIcon icon={faInstagram} size="6x" />
                             </a>
                         </div>
